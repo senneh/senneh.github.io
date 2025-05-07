f866547c550d34d4cb113dc95674db86d99c4896
Senne Hofman <hofmansenne@gmail.com>
2025-05-07 12:52:43 +0200

    setup svelte

---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const config = {
+	preprocess: [vitePreprocess(), mdsvex()],
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	},
+	extensions: ['.svelte', '.svx']
+};
+
+export default config;
